82a802cbb17345c9b27eb985ab4fc5be13fa1d03
EthanVu <evu5917@tamu.edu>
2026-03-28 14:37:15 -0500

    Fix AWS RDS self-signed certificate error on Render

--- project3-team22/server/db.js
+++ project3-team22/server/db.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+// Force Node.js to accept the AWS RDS self-signed certificate in production
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
